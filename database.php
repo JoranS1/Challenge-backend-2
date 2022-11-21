24720fa1076e6e9ac24181e25ac50dfd9ab1b28a
Joran Schrievers <99062987@mydavinci.nl>
2022-11-21 14:11:31 +0100

    Added the files from the first backend challenge into this repo

---
+++ database.php
@@ -0,0 +1,167 @@
+<?php 
+function connAll(){ 
+	try{
+	$conn = new PDO('mysql:host=localhost;dbname=todolist','root', 'mysql');
+	return $conn;
+}
+	catch(PDOException $e){
+	print "Error!: " . $e->getMessage() . "<br/>";
+	die();
+	}
+}  
+function clean($data){
+    $data = preg_replace('@[^A-Za-z0-9\w\ ]@', '', $data);
+    $data = htmlspecialchars($data);
+    $data = stripslashes($data);
+    $data = trim($data);
+    
+    return $data;
+}
+function allTodo(){
+	$conn = connAll();
+	$query = "SELECT * FROM todo ORDER BY id";
+	$conn = connAll();
+	$result = $conn->prepare($query);
+	$result->execute();
+	$rows = $result->fetchAll();
+	return $rows;
+}
+
+function allTask(){
+	$conn = connAll();
+	$query = "SELECT * FROM task ORDER BY id";
+	$conn = connAll();
+	$result = $conn->prepare($query);
+	$result->execute();
+	$rows = $result->fetchAll();
+	return $rows;
+}
+function allTaskOrderdByList($listId){
+	$conn = connAll();
+	$query = "SELECT * FROM task WHERE listId = :listId";
+	$result = $conn->prepare($query);
+	$result->execute([":listId" => $listId]);
+	$rows = $result->fetchAll();
+	return $rows;
+}
+function addTodo($name){
+	$conn = connAll();
+	$query = $conn->prepare("INSERT INTO todo (id, name) VALUES (NULL, :name)");
+	$query -> execute(array(
+		':name' => $name));
+}
+function addTask($name, $description,$time,$listId){
+	
+	$conn = connAll();
+	$query = $conn->prepare("INSERT INTO task (id, name, description, time, status, listId) VALUES (NULL, :name, :description, :time, 1, :listId)");
+	$query -> execute([
+	':name' => $name,
+	':description' => $description,
+	':time' => $time,
+	':listId' => $listId
+]);
+}
+function updateTask($id,$name,$description,$time,$status){
+	$conn = connAll();
+	$query = $conn->prepare("UPDATE task SET name=:name, description=:description, time=:time, status=:status WHERE id=:id");
+	$query->execute([":name" => $name, ":description" => $description, ":time" => $time, ":status" => $status, ":id" => $id]);
+
+	
+}
+function deleteTask($id){
+	$conn = connAll();
+	$query = $conn->prepare("DELETE FROM task WHERE id = ?");
+	$query->execute([$id]);
+}
+function getTodo($id){
+	$conn = connAll();
+	$query = $conn->prepare("SELECT * FROM todo WHERE id=:id");
+	$query->bindParam(":id", $id);
+	$query->execute();
+	$singleTodo = $query->fetch();
+	return $singleTodo;
+}
+function getTask($id){
+	$conn = connAll();
+	$query = $conn->prepare("SELECT * FROM task WHERE id=:id");
+	$query->bindParam(":id", $id);
+	$query->execute();
+	$singleTask = $query->fetch();
+	return $singleTask;
+}
+function updateTodo($id,$name){
+	$conn = connAll();
+	$query = $conn->prepare("UPDATE todo SET name=:name WHERE id=:id");
+	$query->execute([':name' => $name, ':id'=>$id]);
+}
+function deleteTodo($id){
+	$conn = connAll();
+	$query = $conn->prepare("DELETE FROM todo WHERE id = ?");
+	$query->execute([$id]);
+}
+function filterAscStatus(){
+	$conn = connAll();
+	$query = $conn->prepare("SELECT * FROM task ORDER BY status ASC");
+	$query->execute();
+	$result = $query->fetchAll();
+	return $result;
+}
+function filterDescStatus(){
+	$conn = connAll();
+	$query = $conn->prepare("SELECT * FROM task ORDER BY status DESC");
+	$query->execute();
+	$result = $query->fetchAll();
+	return $result;
+}
+function filterStatus(){
+	$conn = connAll();
+	$query = $conn->prepare("SELECT * FROM task WHERE status = 1");
+	$query->execute();
+	$result = $query->fetchAll();
+	return $result;
+}
+/*
+function filterTime(){
+	$conn = connAll();
+	$query = $conn->prepare("SELECT * FROM task ORDER BY time DESC");
+	$query->execute();
+	$result = $query->fetchAll();
+	return $result;
+}*/
+
+//clean functions
+$taskId = clean($_POST['taskId']);
+$todoListId = clean($_POST['todoListId']);
+$taskTime = clean($_POST['taskTime']);
+$taskStatus = clean($_POST['taskStatus']);
+$taskDescription = clean($_POST['taskDescription']);
+$todoName = clean($_POST['todoName']);
+$taskName = clean($_POST['taskName']);
+
+
+
+//post if statements
+if (isset($_POST['makeTodoList'])){
+	addTodo($todoName);
+}
+if(isset($_POST['makeTask'])){
+	
+	addTask($taskName,$taskDescription, $taskTime, $todoListId);
+}
+if(isset($_POST['updateTask'])){
+	
+	updateTask($taskId, $taskName,$taskDescription, $taskTime, $taskStatus);
+}
+if(isset($_POST['deleteTask'])){
+	deleteTask($taskId);
+}
+if (isset($_POST['updateTodo'])){
+	updateTodo($todoListId, $todoName);
+	
+}
+if (isset($_POST['deleteTodo'])){
+	deleteTodo($todoListId);
+}
+
+
+
---
+++ index.php
@@ -0,0 +1,219 @@
+<?php 
+include_once 'database.php'; 
+$todo = allTodo();
+$tasks;
+
+    if(isset($_POST['filterButton'])){
+        filterAscStatus();
+    } 
+    if(isset($_POST['filterDescButton'])){
+        filterDescStatus();
+    }
+    if(isset($_POST['filterActive'])){
+        filterStatus();
+    }
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <title>Todo list</title>
+	<link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script>
+            function modal(elementID, openedClosed){
+                var elementName = document.getElementById(elementID);
+                if(openedClosed === "open"){
+                    elementName.style.display = "block";
+                } else{
+                    elementName.style.display = "none";  
+                }
+            }
+
+            function sortTodo(todo, filter){
+                var todoItems = document.querySelectorAll("[data-todoListID='"+ todo + "']")
+                var sortTodo = [];
+                for(let val of todoItems){
+                    var currentIdName = val.id;
+                    var currentTodoName = val.name;
+                    if(filter === "time"){
+                        var filterCheck = val.dataset.time;
+                    }else if(filter === "status"){
+                        var filterCheck = val.dataset.status;
+                        if(filterCheck === "inactive"){
+                            filterCheck.style.display = "none";
+                        }
+                    } else{
+                        filterCheck = currentTodoName;
+                    }
+
+                    sortTodo.push([currentIdName, filterCheck]);
+                }
+                    sortTodo.sort(function(a, b){
+                        return a[1] - b[1];
+                    });
+
+                    for(let i = 0; i < sortTodo.length; i++){
+                        console.log(sortTodo[i][0]);
+                        document.getElementById(sortTodo[i][0]).style.order = i;
+                    }
+                
+            }
+        </script>
+</head>
+
+<body>
+    <div class="w3-container w3-center">
+        <h2 class="title">Make your own todo list!</h2>
+        <button class="w3-button w3-blue" onclick="modal('modalNewTodo', 'open')">Create a new todo list</button>
+    </div>
+    <hr>
+    <div class="w3-container">
+        <!-- MODAL CREATOR -->
+        
+        <div id="modalNewTodo" class="w3-modal">
+            <div class="w3-modal-content">
+                <div class="w3-container">
+                    <span onclick="modal('modalNewTodo','close')" class="w3-button w3-display-topmiddle">&times;</span>
+
+                    <form action="#" method="post" class="w3-container">
+                        <h3>Create a todo-list</h3>
+                        <input type="text" placeholder="The name of the list..." class="w3-input w3-border" name="todoName"
+                            pattern="[a-zA-Z0-9\s]+" required>
+                       
+                        <br>
+                        <input type="submit" name="makeTodoList" value="Make the Todo list" class="w3-button w3-blue">
+                    </form>
+                    <br>
+                </div>
+            </div>
+        </div>
+
+        <div class="listcard_flex">
+
+        <?php foreach ($todo as $value):?>
+            <div class="w3-card-4" style="display:inline-block; position:relative; height:100%">
+            <header class="w3-container w3-light-grey">
+                <h3><?php echo $value['name'];?></h3>
+            <button class="w3-btn" onclick="sortTodo(<?php echo $value['id']; ?>, 'time')">
+            <i class="fa-fa-clock" aria-hidden="true"></i>
+        </button>
+            <button class="w3-btn" onclick="sortTodo(<?php echo $value['id']; ?>, 'status')">
+            <i class="fa-fa-calendar-check-o" aria-hidden="true"></i>
+            </button>
+            <form method="post" action="#">
+                <input type="submit" name="filterButton" value="filterButton" class="w3-button button">
+                <input type="submit" name="filterDescButton" value="filterDescButton" class="w3-button button">
+                <input type="submit" name="filterActive" value="Active filter" class="w3-button button">
+            </form>
+        
+            </header>
+            <div class="w3-container flex-container" id="todoContainer<?php echo $value['id'];?>">
+            <?php 
+            $task = allTaskOrderdByList($value['id']);
+            foreach($task as $values):?>
+            <div class="task" id="taskId<?php echo $values["id"]; ?>" data-taskName="<?php echo $values['name'];?>" data-taskTime="<?php echo $values['time'];?>" data-taskStatus="<?php echo $values['status']?>" data-todoListId="<?php echo $value["id"];?>">
+                <h3><?php echo $values["name"];?></h3>
+                <p><?php echo $values["description"];?></p>
+                <span class="w3-card w3-purple">Time: <?php echo $values["time"]; ?> min</span>
+                <span class="w3-tag w3-pink"><?php 
+                if ($values['status']){
+                    echo "active";
+                }else{
+                    echo "inactive";
+                }
+                ?></span>
+                <span class="w3-button w3-orange" onclick="modal('modalTask<?php echo $values['id']?>', 'open')">
+            Edit Task <i class="fa-solid fa-gear"></i>
+        </span>
+            </div>
+            <hr>
+
+            <div id="modalTask<?php echo $values['id'] ?>" class="w3-modal">
+                <div class="w3-modal-content">
+                    <div class="w3-container">
+                        <span onclick="modal('modalTask<?php echo $values['id']?>', 'close')" class="w3-button w3-display-topright">&times;</span>
+                        <form action="#" method="post" class="w3-container">
+                            <h3>The task:</h3>
+                            <input type="hidden" name="taskId" value="<?php echo $values['id']?>">
+                            <br>
+                            <input type="text" name="taskName" placeholder="Name of the task" class="w3-input w3-border" pattern="[a-zA-Z0-9\s]+" required value="<?php echo $values['name']?>">
+                            <br>
+                            <input type="text" name="taskDescription" placeholder="Description of the task" class="w3-input w3-border" pattern="[a-zA-Z0-9\s]+" required value=<?php echo $values['description']?> class="w3-input w3-border">
+                            <br>
+                            <input type="number" name="taskTime" placeholder="Duration of the task (in minutes please)" class="w3-input w3-border" required value=<?php echo $values['time']?>>
+                            <br>
+                            <label for="status">Task Status</label>
+                            <input type="radio" name="taskStatus" value="1" <?php if($values['status']){
+                                echo 'Checked';
+                            } ?>>
+                            <label>Active</label><br>
+                            <input type="radio" name="taskStatus" value="0" <?php if(!$values['status']){
+                                echo 'Checked';
+                            } ?>>
+                            <label>Inactive</label><br>
+                            <input type="submit" name="updateTask" value="Update Task" class="w3-btn w3-block">
+                            <input type="submit" name="deleteTask" value="Delete Task" class="w3-btn w3-red w3-block">
+                            <br>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <?php endforeach;?>
+        
+        
+        </div>
+        <br>
+        <div style="position: relative; bottom: 0;">
+                <button class="w3-button w3-block w3-purple" onclick="modal('modalEditTodo<?php echo $value['id']?>', 'open')">Edit the Todo List</button>
+                <button class="w3-button w3-block w3-green" onclick="modal('modalNewTask<?php echo $value['id']?>', 'open')">New task</button>
+    </div>
+    <div id="modalNewTask<?php echo $value['id']?>" class="w3-modal">
+        <div class="w3-modal-content">
+            <div class="w3-container">
+            <span onclick="modal('modalNewTask<?php echo $value['id']?>', 'close')" cols="25" rows="10" class="w3-button w3-display-topright">&times;</span>
+
+            <form action="#" method="post" class="w3-container">
+                <h3>New task</h3>
+                <input type="hidden" name="todoListId" value="<?php echo $value['id']?>">
+                <br>
+                <input type="text" name="taskName" placeholder="Name of the task" class="w3-input w3-border" pattern="[a-zA-Z0-9\s]+" required>
+                <br>
+                <input type="text" name="taskDescription" style="resize: vertical;" class="w3-input w3-border" placeholder="Description of the task" pattern="[a-zA-Z0-9\s]+" required>
+                <br>
+                <input type="number" name="taskTime" class="w3-input w3-border" placeholder="Duration of the task (in minutes please)" class="w3-input w3-border" required>
+                <br>
+                <input type="submit" name="makeTask" value="Add task" class="w3-btn w3-block">
+                <br>
+            </form>
+            </div>
+
+        </div>
+</div>
+        </div>
+        <div id="modalEditTodo<?php echo $value['id']?>" class="w3-modal">
+        <div class="w3-modal-content">
+            <div class="w3-container">
+            <span onclick="modal('modalEditTodo<?php echo $value['id']?>', 'close')" cols="25" rows="10" class="w3-button w3-display-topright">&times;</span>
+            <form action="#" method="post" class="w3-container">
+                <h3>Edit Todo List</h3>
+                <input type="hidden" name="todoListId" value="<?php echo $value['id']?>">
+                <br>
+                <input type="text" name="todoName" placeholder="Name of the todo list" class="w3-input w3-border" pattern="[a-zA-Z0-9\s]+" required value="<?php echo $value['name']?>">
+                <input type="submit" name="updateTodo" value="Update Todo List" class="w3-btn w3-block">
+                <input type="submit" name="deleteTodo" value="Delete Todo List" class="w3-btn w3-red w3-block">
+            </form>
+            </div>
+            </div>
+            </div>
+        <?php endforeach;?>
+        </div>
+    </div>
+    <script src="jquery.js"></script>
+</body>
+</html>
